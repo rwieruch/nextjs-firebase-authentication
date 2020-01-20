ce7e32fd91716fc220f82914798aedb85779772e
Robin Wieruch <wrobin@gmx.net>
2020-01-20 22:43:20 +0100

    sign out

--- src/components/Navigation/signOut.ts
+++ src/components/Navigation/signOut.ts
@@ -3,9 +3,12 @@ import cookie from 'js-cookie';
 
 import { ServerRequest, ServerResponse } from '@typeDefs/server';
 import * as ROUTES from '@constants/routes';
+import firebase from '@services/firebase';
 
 export default (req?: ServerRequest, res?: ServerResponse) => {
   const isServer = req || res;
+
+  firebase.auth().signOut();
 
   if (isServer) {
     res?.writeHead(302, { Location: ROUTES.SIGN_IN });
