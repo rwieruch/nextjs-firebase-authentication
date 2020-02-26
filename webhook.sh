2c2d9696a1e30df4adb42768710743cde609de80
Robin Wieruch <wrobin@gmx.net>
2020-02-26 18:05:27 +0100

    adjust webhook

--- webhook.sh
+++ webhook.sh
@@ -1,7 +1,9 @@
 git pull --rebase origin master
 npm install
-cd content
+cd src/data/courses
 git pull --rebase origin master
+cd ..
+cd ..
 cd ..
 npm run build
 pm2 restart courses.robinwieruch.de
