cc825adddd9d4d526fb3e030109aa3209837f554
Robin Wieruch <wrobin@gmx.net>
2020-03-24 10:58:44 +0100

    postgres with typeorm setup

---
+++ src/models/course.ts
@@ -0,0 +1,41 @@
+import {
+  Index,
+  Entity,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { COURSE } from '@data/course-keys';
+import { BUNDLE } from '@data/bundle-keys';
+
+@Entity()
+export class Course {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Index()
+  @Column('varchar')
+  userId: string;
+
+  @Column('varchar')
+  courseId: COURSE;
+
+  @Column('varchar')
+  bundleId: BUNDLE;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column('int')
+  price: number;
+
+  @Column('varchar')
+  currency: string;
+
+  @Column('varchar')
+  paymentType: string;
+
+  @Column('varchar')
+  coupon: string;
+}
