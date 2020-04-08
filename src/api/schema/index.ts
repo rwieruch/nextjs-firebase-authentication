21495e2d3b4512d234f25064063ca9a874635068
Robin Wieruch <wrobin@gmx.net>
2020-04-08 14:23:01 +0200

    sentry middleware before authroization

--- src/api/schema/index.ts
+++ src/api/schema/index.ts
@@ -1,5 +1,32 @@
 import { mergeSchemas } from 'graphql-tools';
 import { applyMiddleware } from 'graphql-middleware';
+import { sentry } from 'graphql-middleware-sentry';
+import * as Sentry from '@sentry/node';
+
+import { ResolverContext } from '@typeDefs/resolver';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+});
+
+const sentryMiddleware = sentry({
+  sentryInstance: Sentry,
+  config: {
+    environment: process.env.NODE_ENV,
+  },
+  forwardErrors: true,
+  captureReturnedErrors: true,
+  withScope: (scope, error, context: ResolverContext) => {
+    scope.setUser({
+      id: context.me?.uid,
+      email: context.me?.email,
+    });
+
+    scope.setExtra('body', context.req.body);
+    scope.setExtra('origin', context.req.headers.origin);
+    scope.setExtra('user-agent', context.req.headers['user-agent']);
+  },
+});
 
 import { Resolvers } from '@generated/server';
 
@@ -12,4 +39,8 @@ const schema = mergeSchemas({
   resolvers: resolvers as Resolvers,
 });
 
-export default applyMiddleware(schema, authorization);
+export default applyMiddleware(
+  schema,
+  authorization,
+  sentryMiddleware
+);
